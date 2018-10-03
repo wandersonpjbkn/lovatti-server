d9caf230dff0b334c1366c46784125b5dffa491a
Web <web@perfectimage.com.br>
2018-10-03 15:02:20 -0300

    Fixed CORS origin
    - Match to Heroku subdomain

--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,7 @@
 export default {
     port: process.env.PORT || '3000',
     cors: {
-        origin: (origin, next) => {
-            let domain = [
-                'http://lovatticonfeccoes.com.br/',
-                'http://www.lovatticonfeccoes.com.br/',
-                'https://lovatticonfeccoes.com.br/',
-                'https://www.lovatticonfeccoes.com.br/'
-            ]
-            if (!~domain.indexOf(origin)) {
-                let msg = 'Não é permitido requisições desta origem'
-                return next(new Error(msg), false)
-            }
-            return next(null, true)
-        },
+        origin: 'https://lovatti-server.herokuapp.com/',
         optionsSuccessStatus: 200
     }
 }
