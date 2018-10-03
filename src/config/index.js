76734f3d335a585cfd83c03dd5f5ab37a96652a0
Web <web@perfectimage.com.br>
2018-10-03 15:04:59 -0300

    Fixed CORS

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     port: process.env.PORT || '3000',
     cors: {
-        origin: 'https://lovatti-server.herokuapp.com/',
+        origin: 'https://lovatti-server.herokuapp.com/send',
         optionsSuccessStatus: 200
     }
 }
